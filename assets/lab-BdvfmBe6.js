da2e4152a3e4dd39af714a7b65c38e8b8f2ad80f
spiliour <spiliour@users.noreply.github.com>
2026-07-21 21:31:10 +0000

    deploy: 2df4b515d17a4ff1b404dbd8f116ff10adfad88f

---
+++ assets/lab-BdvfmBe6.js
@@ -0,0 +1,1 @@
+import{bd as t,j as e,be as o,bf as r}from"./index-DC706D6u.js";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{})}));
--- assets/lab-C5trrBwE.js
+++
@@ -1,1 +0,0 @@
-import{bd as t,j as e,be as o,bf as r}from"./index-DUKougbK.js";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{})}));
