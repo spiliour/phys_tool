8bded80852265f66219a5432dfab6b49c97d6df1
spiliour <spiliour@users.noreply.github.com>
2026-07-27 15:45:22 +0000

    deploy: f8c20505642aec28fedc8af900fa6a41403c0bce

--- assets/lab-BdvfmBe6.js
+++
@@ -1,1 +0,0 @@
-import{bd as t,j as e,be as o,bf as r}from"./index-DC706D6u.js";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{})}));
---
+++ assets/lab-C-K8VNWP.js
@@ -0,0 +1,1 @@
+import{bj as t,j as e,bk as o,bl as r}from"./index-ThKBEcSs.js";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{})}));
