ff4e0c77e632542d4e7e2c99deeebdb4282bf220
spiliour <spiliour@users.noreply.github.com>
2026-07-28 12:38:13 +0000

    deploy: 52d45d5a52c2d439ca86f849061cd039a84c670b

---
+++ assets/lab-CHYB9-tO.js
@@ -0,0 +1,1 @@
+import{bn as t,j as e,bo as o,bp as r}from"./index-COlh91ln.js";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{})}));
--- assets/lab-CgcygNEm.js
+++
@@ -1,1 +0,0 @@
-import{bn as t,j as e,bo as o,bp as r}from"./index-DIrzA_l1.js";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{})}));
